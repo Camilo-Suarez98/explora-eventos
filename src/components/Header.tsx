06ab7b7190c40a2596df37d842ad29a2a7f56554
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-28 08:39:45 -0500

    fix header image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ export const Header = () => {
   return (
     <nav className="bg-white shadow-lg">
       <div className="mx-auto px-4">
-        <div className="flex justify-center">
+        <div className="flex justify-center items-center">
           <div className="flex">
             <div className="flex items-center py-4">
               <img
-                src="./logo-explora.png"
+                src="../../public/logo-explora.png"
                 alt="Explora Eventos Logo"
                 className="w-48"
               />
