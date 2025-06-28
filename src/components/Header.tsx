260bd7840698cb1f1f773ed049159eef93cfa7cc
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-27 20:05:11 -0500

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="mx-auto px-4">
+        <div className="flex justify-center">
+          <div className="flex">
+            <div className="flex items-center py-4">
+              <img
+                src="./logo-explora.png"
+                alt="Explora Eventos Logo"
+                className="w-48"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
