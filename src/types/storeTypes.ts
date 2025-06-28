2a71c88229d6d75a06e7ba853ec7fcf284b8614c
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-28 08:48:56 -0500

    create filter by date

--- src/types/storeTypes.ts
+++ src/types/storeTypes.ts
@@ -5,8 +5,10 @@ export interface Store {
   filteredEvents: Event[];
   searchTerm: string;
   selectedType: 'all' | EventType;
+  selectedDate: string;
   setEvents: (events: Event[]) => void;
   setSearchTerm: (term: string) => void;
   setSelectedType: (type: 'all' | EventType) => void;
+  setSelectedDate: (date: string) => void;
   filterEvents: () => void;
 }
